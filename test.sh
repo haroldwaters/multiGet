5ea077b9cae9e6fb5f9a85d139743d629be89daa
Harold <waters.harold@gmail.com>
2018-05-06 22:44:24 -0500

    Renamed index.js to multiGet.js

--- test.sh
+++ test.sh
@@ -1,25 +1,25 @@
 target='http://40ff26ef.bwtest-aws.pravala.com/384MB.jar'
 
 echo 'Testing with 1 chunk'
-node index.js $target -c 1
-diff -s downloads/test.txt testfile/384MB.jar
+node multiGet.js $target -c 1
+diff -s downloads/384MB.jar testfile/384MB.jar
 
 echo 'Testing with 2 chunks'
-node index.js $target -c 2
-diff -s downloads/test.txt testfile/384MB.jar
+node multiGet.js $target -c 2
+diff -s downloads/384MB.jar testfile/384MB.jar
 
-echo 'Testing with 4 chunks'
-node index.js $target -c 4
-diff -s downloads/test.txt testfile/384MB.jar
+echo 'Testing with 7 chunks'
+node multiGet.js $target -c 7
+diff -s downloads/384MB.jar testfile/384MB.jar
 
 echo 'Testing with 1 chunk, 10000 bytes'
-node index.js $target -c 1 -s 10000
-cmp -n 1000 -bl downloads/test.txt testfile/384MB.jar
+node multiGet.js $target -c 1 -s 10000
+cmp -n 1000 -bl downloads/384MB.jar testfile/384MB.jar
 
 echo 'Testing with 2 chunks, 10000 bytes'
-node index.js $target -c 2 -s 10000
-cmp -n 10000 -bl downloads/test.txt testfile/384MB.jar
+node multiGet.js $target -c 2 -s 10000
+cmp -n 10000 -bl downloads/384MB.jar testfile/384MB.jar
 
-echo 'Testing with 4 chunks, 10000 bytes'
-node index.js $target -c 4 -s 10000
-cmp -n 10000 -bl downloads/test.txt testfile/384MB.jar
+echo 'Testing with 7 chunks, 10000 bytes'
+node multiGet.js $target -c 7 -s 10000
+cmp -n 10000 -bl downloads/384MB.jar testfile/384MB.jar
