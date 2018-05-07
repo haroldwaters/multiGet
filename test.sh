220a587393cedc53314f13e5f65c4a44569df09c
Harold <waters.harold@gmail.com>
2018-05-07 12:40:38 -0500

    test.sh now downloads file for comparison and removes when finished

--- test.sh
+++ test.sh
@@ -1,25 +1,35 @@
 target='http://40ff26ef.bwtest-aws.pravala.com/384MB.jar'
+testfiledest='./testfile/384MB.jar'
+downloaddest='./downloads/384MB.jar'
+chunksize=10000
 
-echo 'Testing with 1 chunk'
-node multiGet.js $target -c 1
-diff -s downloads/384MB.jar testfile/384MB.jar
+if [ ! -e $testfiledest ]; then
+    echo 'Comparison file not found, downloading it now'
+    wget http://40ff26ef.bwtest-aws.pravala.com/384MB.jar --output-document=$testfiledest
+fi
 
-echo 'Testing with 2 chunks'
-node multiGet.js $target -c 2
-diff -s downloads/384MB.jar testfile/384MB.jar
+# echo 'Testing with 1 chunk'
+# node multiGet.js $target -c 1
+# diff -s $downloaddest $testfiledest
 
-echo 'Testing with 7 chunks'
-node multiGet.js $target -c 7
-diff -s downloads/384MB.jar testfile/384MB.jar
+# echo 'Testing with 2 chunks'
+# node multiGet.js $target -c 2
+# diff -s $downloaddest $testfiledest
 
-echo 'Testing with 1 chunk, 10000 bytes'
-node multiGet.js $target -c 1 -s 10000
-cmp -n 1000 -bl downloads/384MB.jar testfile/384MB.jar
+# echo 'Testing with 7 chunks'
+# node multiGet.js $target -c 7
+# diff -s $downloaddest $testfiledest
 
-echo 'Testing with 2 chunks, 10000 bytes'
-node multiGet.js $target -c 2 -s 10000
-cmp -n 10000 -bl downloads/384MB.jar testfile/384MB.jar
+# echo 'Testing with 1 chunk, 10000 bytes'
+# node multiGet.js $target -c 1 -s $chunksize
+# cmp -n $chunksize -bl $downloaddest $testfiledest
+
+# echo 'Testing with 2 chunks, 10000 bytes'
+# node multiGet.js $target -c 2 -s $chunksize
+# cmp -n $chunksize -bl $downloaddest $testfiledest
 
 echo 'Testing with 7 chunks, 10000 bytes'
-node multiGet.js $target -c 7 -s 10000
-cmp -n 10000 -bl downloads/384MB.jar testfile/384MB.jar
+node multiGet.js $target -c 7 -s $chunksize
+cmp -n $chunksize -bl $downloaddest $testfiledest
+
+rm $testfiledest
