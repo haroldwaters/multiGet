9831a2c8b5623af3ddbbd98ab970c486ff71b7a7
Harold <waters.harold@gmail.com>
2018-05-06 22:42:16 -0500

    test.sh now checks partial downloads

--- test.sh
+++ test.sh
@@ -12,10 +12,14 @@ echo 'Testing with 4 chunks'
 node index.js $target -c 4
 diff -s downloads/test.txt testfile/384MB.jar
 
-echo 'Testing with 8 chunks'
-node index.js $target -c 8
-diff -s downloads/test.txt testfile/384MB.jar
+echo 'Testing with 1 chunk, 10000 bytes'
+node index.js $target -c 1 -s 10000
+cmp -n 1000 -bl downloads/test.txt testfile/384MB.jar
 
-echo 'Testing with 16 chunks'
-node index.js $target -c 16
-diff -s downloads/test.txt testfile/384MB.jar
+echo 'Testing with 2 chunks, 10000 bytes'
+node index.js $target -c 2 -s 10000
+cmp -n 10000 -bl downloads/test.txt testfile/384MB.jar
+
+echo 'Testing with 4 chunks, 10000 bytes'
+node index.js $target -c 4 -s 10000
+cmp -n 10000 -bl downloads/test.txt testfile/384MB.jar
